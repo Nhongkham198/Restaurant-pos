a90a53cd97b3fece8c45a19a3978d5b0fe94dfcc
Nhongkham198 <zowamarketing@gmail.com>
2026-01-27 09:24:35 +0700

    En mobile 5

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -92,11 +92,14 @@ export const MenuItemCard: React.FC<MenuItemCardProps> = ({
             </div>
 
             {isRecommended && (
-                <div className="absolute top-2 right-2 flex items-center justify-center w-8 h-8 z-20 pointer-events-none" title="เมนูแนะนำ">
-                    <div className="absolute w-full h-full bg-yellow-400 rounded-full animate-ping opacity-75"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="relative h-7 w-7 text-yellow-500 drop-shadow-lg" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
+                <div className="absolute top-2 left-2 flex items-center justify-center w-10 h-10 z-20 pointer-events-none" title="เมนูแนะนำ">
+                    {/* Anime-like effect: Ping animation behind + Bounce animation on icon */}
+                    <div className="absolute w-full h-full bg-red-500/40 rounded-full animate-ping"></div>
+                    <div className="relative bg-white rounded-full p-1.5 shadow-lg border-2 border-red-500 flex items-center justify-center animate-bounce">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-600" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                        </svg>
+                    </div>
                 </div>
             )}
 
