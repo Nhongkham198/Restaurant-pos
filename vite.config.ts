f725cfa7329d865ef097ea165430e9a5130debf1
Nhongkham198 <zowamarketing@gmail.com>
2026-03-06 10:30:46 +0700

    facebook connect 1.3

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Force rebuild for static files
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
