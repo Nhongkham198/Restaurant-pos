b5f9efb54638f29e04260da65f792284b1b6b2d8
Nhongkham198 <zowamarketing@gmail.com>
2026-05-08 09:24:39 +0700

    edit Recipes

--- components/RecipeManagement.tsx
+++ components/RecipeManagement.tsx
@@ -939,8 +939,8 @@ export const RecipeManagement: React.FC<RecipeManagementProps> = ({
                                                     </span>
                                                 )}
                                             </div>
-                                            {!recipeStatus.hasUpdate && isUpToDate && (
-                                                <div className="flex-shrink-0 bg-green-100 text-green-700 p-0.5 rounded-full" title="ราคาวันนี้ซิงค์กับ JSON แล้ว">
+                                            {!recipeStatus.hasUpdate && recipe && (
+                                                <div className="flex-shrink-0 bg-green-100 text-green-700 p-0.5 rounded-full" title="สูตรอาหารถูกต้องและบันทึกแล้ว">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                                     </svg>
